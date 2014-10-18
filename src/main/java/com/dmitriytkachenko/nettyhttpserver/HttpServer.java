a1141251a727da680ac386a386802af16e7e9ac8
WildSpirit <wildspirit1994@gmail.com>
2014-10-18 21:35:31 +0300

    Added HttpServer and HttpServerInitializer classes.

--- src/main/java/com/dmitriytkachenko/nettyhttpserver/HttpServer.java
+++ src/main/java/com/dmitriytkachenko/nettyhttpserver/HttpServer.java
@@ -1,7 +1,58 @@
 package com.dmitriytkachenko.nettyhttpserver;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
 public final class HttpServer {
-    public static void main(String[] args) {
-        
+    private int port;
+
+    public int getPort() {
+        return port;
     }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public HttpServer() {
+    }
+
+    public HttpServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8888;
+        }
+        new HttpServer(port).run();
+    }
+
+    public void run() throws Exception {
+        // Configure the server.
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HttpServerInitializer());
+
+            Channel ch = b.bind(port).sync().channel();
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
 }
---
+++ src/main/java/com/dmitriytkachenko/nettyhttpserver/HttpServerInitializer.java
@@ -0,0 +1,12 @@
+package com.dmitriytkachenko.nettyhttpserver;
+
+import io.netty.channel.ChannelInitializer;
+
+import java.nio.channels.SocketChannel;
+
+public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+    }
+}
